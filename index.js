9526498fb85f8efc07b7ed06fefc2070822c7c16
Janos Perge <janos.perge@optimonk.com>
2021-10-27 21:50:19 +0200

    finish case 5

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 function toRoman(a) {
 
+    if(a % 5 === 0 ){
+        return drawSymbol(1, 'V');
+    }
+    if(a === 4){
+        return drawSymbol(1, 'I') + drawSymbol(1, 'V')
+    }
     if(a <= 3 ){
-        return symbolI(a);
-    } else {
-        return 'IV';
+        return drawSymbol(a, 'I');
     }
 
     
 }
 
-function symbolI(num){
-    const symbol = 'I';
+function drawSymbol(num, sym){
+    const symbol = sym;
     let returnSymbol = '';
     for (let i = 0; i < num; i++) {
         returnSymbol += symbol;
