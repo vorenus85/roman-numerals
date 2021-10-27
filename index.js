ca107aa0479921669ddae36c82ab373f375b0456
Janos Perge <janos.perge@optimonk.com>
2021-10-27 23:21:05 +0200

    finish to case 15.. need some refactoring :)

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
+const I = drawSymbol(1, 'I');
+const V = drawSymbol(1, 'V');
+const X = drawSymbol(1, 'X');
 function toRoman(a) {
-
-    const I = drawSymbol(1, 'I');
-    const V = drawSymbol(1, 'V');
-    const X = drawSymbol(1, 'X');
     const modulo = a % 5;
-    if(a % 10 === 0 && a % 2 === 0){
-        return X;
-    }
-    if(a % 5 === 0 ){
+    const moduloTen = a % 10;
+    if(modulo === 0 ){
+        if(moduloTen === 0){
+            return X;
+        }
+        if(a > 10){
+            return X+V;
+        }
         return V;
     }
+
     if(modulo === 4){
-        if (a > 5){
-            return I + X;
-        } else {
-            return I + V;
-        }
+        return drawModuloFourSymbols(a);
     }
-    if(a > 5 ){
+
+    if(a > moduloTen) {
+        return X + drawSymbol(modulo, 'I');
+    }
+    else if(a > modulo) {
         return V + drawSymbol(modulo, 'I');
+    } else {
+        return drawSymbol(modulo, 'I');
     }
-    if(a <= 3 ){
-        return drawSymbol(a, 'I');
-    } 
 }
 
 function drawSymbol(num, sym){
@@ -32,6 +35,22 @@ function drawSymbol(num, sym){
         returnSymbol += symbol;
     }
     return returnSymbol;
+}
+
+function drawModuloFourSymbols(a){
+    switch (a) {
+        case 4:
+            return I+V;
+            break;
+        case 9:
+            return I+X;
+            break;
+        case 14:
+            return X+I+V;
+            break;
+        default:
+            break;
+    }
 }
 
 module.exports = toRoman;
